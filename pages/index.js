f63d66de4fe87a3a8b681519ccb1f1133f66f3d5
Donathan Queiros <donathanbt@gmail.com>
2021-02-03 20:59:15 -0300

    Link Resolvido nextjs

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,10 @@ import Link from 'next/link';
 function Home() {
     return (
         <div>
-<<<<<<< HEAD
             <h1>Home</h1>
             <Link href="sobre">
                 <a>Acessar página Sobre</a>
             </Link>
-=======
-            <h1>Pagina inicial</h1>
->>>>>>> b9e0392a086fc1c5d989d8d1ba1804c309af9565
         </div>
     )
 }
